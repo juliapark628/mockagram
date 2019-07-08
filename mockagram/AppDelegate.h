735facff0bf04c09113ccf199438b4d6210c2c44
Julia Park <juliapark628@fb.com>
2019-07-08 15:08:40 -0700

    Initial Commit

---
+++ mockagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mockagram
+//
+//  Created by juliapark628 on 7/8/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mockagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mockagram
+//
+//  Created by juliapark628 on 7/8/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
